b57633125cd61400df813ab0359b8a5cf47de4b9
luciaharcegova <fakeluciamail@gmail.com>
2022-01-04 18:16:40 +0100

    connected article with user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_124915) do
+ActiveRecord::Schema.define(version: 2022_01_04_170625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_01_03_124915) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
     t.string "subtitle"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
