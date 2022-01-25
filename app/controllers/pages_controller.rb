b11bbede1d5f24f6c044eac9833337f025e2af71
Lucia Harcegova <fakeluciamail@gmail.com>
2022-01-25 09:56:47 +0100

    adjusted the homepage view to display newest articles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
-    # debugger
+    articles = Article.all.order(created_at: :desc)
+    @article_newest = articles[0]
+    @article_second_newest = articles[1]
   end
 end
