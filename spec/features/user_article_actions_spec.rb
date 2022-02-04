038eb175767f7ac89e2c0bfdc3c6ff3d15b460cf
Lucia Harcegova <fakeluciamail@gmail.com>
2022-02-04 08:56:44 +0100

    spces for logged-in user view

--- spec/features/user_article_actions_spec.rb
+++ spec/features/user_article_actions_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.feature "UserArticleActions", type: :feature do
   context "user is logged in" do
-    scenario "creates article" do
-      visit new_article_path
+    before(:all) do
+      @user = build(:user)
+    end
+    scenario "creates an article" do
+      visit articles_new_path
 
+      fill_in "article[title]", with: "Example"
+      fill_in "Subtitle", with: "Example"
+      fill_in_trix_editor "article_rich_body_trix_input_article", "Example text"
+      fill_in "Category", with: "Example"
+      click_on "Post"
+
+      expect(page).to have_css ".info-line small p", Text: @user.username
     end
   end
 
