cf580b8dd7f3276f44e3d3be88aad9d9cedbd307
Lucia Harcegova <fakeluciamail@gmail.com>
2022-12-22 20:28:20 +0100

    Home fix, add edit article

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     articles = Article.all.order(created_at: :desc)
-    @article_newest = articles[0]
-    @article_second_newest = articles[1]
+    @article_newest = articles.first
+    @article_second_newest = articles.second
   end
 end
